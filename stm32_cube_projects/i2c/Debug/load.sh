dd0d7b893e2e96867a7521a55fb062c45c2ebd78
lord-feistel <lord.feistel@gmail.com>
2024-06-04 15:48:28 +0200

    excluding useless files

--- stm32_cube_projects/i2c/Debug/load.sh
+++
@@ -1,3 +0,0 @@
-arm-linux-gnueabi-objcopy -O binary  i2c.elf i2c.bin
-st-flash write i2c.bin 0x8000000
-
--- stm32_cube_projects/i2c/Debug/openocd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=/usr/share/openocd/scripts
-openocd -f $SCRIPT_DIR/interface/stlink-v2.cfg  -f $SCRIPT_DIR/target/stm32f1x.cfg
-
